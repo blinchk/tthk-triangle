52b0ec9644492d87d048a29391aaa0fbec5433ff
Nikolas Laus <bchik3@gmail.com>
2020-05-19 20:00:23 +0300

    Добавьте файлы проекта.

---
+++ Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tthk_triangle
+{
+    class Triangle
+    {
+        public double a, b, c;
+
+        static Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+    }
+}
